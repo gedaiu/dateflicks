f39a76c6cf78b4c84cf4102a610abbafd248cbb8
Szabo Bogdan <szabobogdan@yahoo.com>
2018-03-07 15:18:07 +0200

    create a failing post session test

---
+++ tests/session/rest.js
@@ -0,0 +1,26 @@
+const SessionService = require("../../services/session/service");
+var should = require('should');
+
+describe("The session service REST API", function () {
+  var service;
+
+  before(function () {
+    service = new SessionService({ port: 3000 });
+  });
+
+  it("should create a session", function (done) {
+    service.server.inject({
+      method: 'POST',
+      url: '/sessions',
+      payload: {
+        'session': {
+          "hostUserId": 1,
+          "guestUserId": 2,
+          "videoId": 1
+        }
+      }
+    }).then(response => {
+      response.statusCode.should.equal(200);
+    }).catch(done);
+  });
+});
