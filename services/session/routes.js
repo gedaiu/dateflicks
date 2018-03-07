f86a1301b202c8a5d3f20460587fe6c9d43e89c1
Szabo Bogdan <szabobogdan@yahoo.com>
2018-03-07 17:25:18 +0200

    add route file

---
+++ services/session/routes.js
@@ -0,0 +1,27 @@
+const SessionModel = require("../../models/session");
+var mongoose = require('mongoose');
+
+module.exports = {
+  setup: function (server, notifications) {
+
+    server.route({
+      path: '/sessions',
+      method: 'POST',
+      handler: async function (request, h) {
+        request.payload.session.hostUserId = new mongoose.mongo.ObjectId(request.payload.session.hostUserId);
+        request.payload.session.guestUserId = new mongoose.mongo.ObjectId(request.payload.session.guestUserId);
+        request.payload.session.videoId = new mongoose.mongo.ObjectId(request.payload.session.videoId);
+
+        try {
+          const result = await new SessionModel(request.payload.session).save();
+          notifications.emit("newSession", result);
+
+          return { session: result };
+        } catch (err) {
+          console.log(err);
+          return err;
+        }
+      }
+    });
+  }
+}
