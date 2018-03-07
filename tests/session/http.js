24787e874931aea883d014528cb6fbbcd77f750d
Szabo Bogdan <szabobogdan@yahoo.com>
2018-03-07 14:58:41 +0200

    write test for the session server init

--- tests/session/http.js
+++ tests/session/http.js
@@ -1,4 +1,6 @@
 const SessionService = require("../../services/session/service");
+const http = require('http');
+var should = require('should');
 
 describe("The session service", function() {
   var service;
@@ -10,8 +12,14 @@ describe("The session service", function() {
     }
   });
 
-  it("should start on a provided port", function() {
+  it("should start on a provided port", function(done) {
     service = new SessionService({ port: 3000 });
     service.start();
+    const url = 'http://' + service.server.info.host + ':' + service.server.info.port;
+
+    http.get(url, (resp) => {
+      resp.statusCode.should.eql(404);
+      done();
+    }).on("error", done);
   });
 });
